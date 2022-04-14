d3824f3b00a90b3157b9f47313af856cb92f0957
muthioo-faith <muthioofaith@gmail.com>
2022-04-14 09:31:46 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,40 @@
+import java.util.jar.Attributes
+
+fun main(){
+    var CurrentAccount=CurrentAccount(26783,"Mutua faith",20,000)
+
+    details()
+}
+class CurrentAccount(var accountnumber:Int,var accountname:String, var balance:Int) {
+    fun deposit(amount: Double): Double {
+        var totalAmount =amount*balance
+        return totalAmount++
+    }
+    fun withdrawal(amount:Double):Double {
+        var amount=20,000
+        var withdrawalamount = amount
+
+}
+}
+fun details(){
+    println("Account number $26783 with balance $20,000 operated by $ name Mutua Faith")
+}
+}
+class SavingsAccount( var accountnumber: Int,var accountname: String,var withdrawals:Int){
+    var number=0
+
+
+}
+data class(var name:String,var weight:Int,var price:Int,){
+    var category=(x is{ ->
+
+
+}
+fun output(name: String) {
+    var name = "Mutua"
+    var x = (name[1].toString() + name[2] + name[3])
+    println(x)
+
+}
+
+
